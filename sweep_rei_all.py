23f05d5516d7fe63f5aa992b5ca98f4222d5b950
Dylan Payne <11635831+DylanPayne@users.noreply.github.com>
2023-08-23 12:11:33 -0600

    full production run, no errors!

--- sweep_rei_all.py
+++ sweep_rei_all.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+import sys
+from os.path import dirname, join
+venv_path = join(dirname(__file__), 'venv')  # Path to your virtual environment
+activate_this = join(venv_path, 'bin/activate_this.py')
+with open(activate_this) as file_:
+    exec(file_.read(), dict(__file__=activate_this))
+
 import os, argparse, logging, time, random, json
 from datetime import datetime, timezone
 from utils.db_utils import DatabaseInserter, table_schema_rei_sweep
@@ -5,27 +13,32 @@ from utils.scrape_utils import rei_sweep_filter_conditions, fetch_rei_sweep_api
 from rei.rei_parser import parse_rei_sweep_page, parse_rei_sweep_all
 from utils.log_config import log_config
 
-def main(suffix=""):
+def main(prefix):
     # Log the start time
     start_time = time.time()
-    run_id = datetime.utcnow() # Save run_id as datetime
-    logger = log_config("scrape_rei_all.log")
-    logger.info(f"\nScript started: {run_id}")
+    
+    script_name = os.path.basename(os.path.abspath(__file__))
+    run_name = os.path.splitext(script_name)[0]
+    
+    logger = log_config(f"{run_name}.log")
+    logger.info(f"/n Starting {script_name}")
     logging.getLogger('sqlalchemy.engine').setLevel(logging.ERROR)
     
-    # Create tables in postgresql, with naming dependant on optional argparse suffix
+    # Create tables in postgresql, naming incorporates optional argparse prefix
     try:
         with DatabaseInserter() as db_conn:
             for table_name, schema in table_schema_rei_sweep.items():
-                db_conn.create_table(f'{table_name}{suffix}', schema, logger) # create tables
+                db_conn.create_table(f'{prefix}{table_name}', schema, logger) # create tables
+            run_id = db_conn.start_run(run_name, prefix, logger) # start run by inserting row
+            logger.info(f"\n {script_name} started run_id: {run_id}")
     except Exception as e:
         logger.error(f"Error creating rei_sweep tables: {e}")
             
-    # Determine the output table names based on optional suffix input
-    items_table_name = f'rei_sweep_all{suffix}'
-    page_table_name = f'rei_sweep_page{suffix}'
+    # Determine output table names based on optional prefix input
+    items_table_name = f'{prefix}rei_sweep_all'
+    page_table_name = f'{prefix}rei_sweep_page'
 
-    item_cap = 100 # set to None for unpage_limited. Per filter
+    item_cap = None # set to None for unpage_limited. Per filter
     page_limit = 100
     
     for filter in rei_sweep_filter_conditions:
@@ -43,23 +56,23 @@ def main(suffix=""):
 
             try:
                 with DatabaseInserter() as db_conn:
-                    db_conn.insert_to_sql(df_items, items_table_name, helper_columns, logger)
-                    db_conn.insert_to_sql(df_page, page_table_name, helper_columns, logger)
+                    db_conn.insert_to_sql(df_items, items_table_name, logger, helper_columns)
+                    db_conn.insert_to_sql(df_page, page_table_name, logger, helper_columns)
             except Exception as e:
                 logger.error(f"Error inserting data into database {str(e)}")
             
             offset += page_limit # increment to next page
             item_count = df_page['count'].item() # Assign item_count so it knows when to stop paginating
-            time.sleep(random.uniform(2,6)) # wait between 1-5 seconds
+            time.sleep(random.uniform(2,8)) # wait x-y seconds
     
     # Calculate and log the time elapsed
     elapsed_time = round((time.time() - start_time) / 60, 1)
     logger.info(f"Script completed. Time elapsed: {elapsed_time} minutes.")
     
-# argparse allows you to pass in suffix: python scrape_rei_all.py --suffix=test
+# argparse allows you to pass in prefix: python scrape_rei_all.py --prefix=test
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Scrape and save REI data")
-    parser.add_argument("--suffix", help="Optional suffix for table names")
+    parser.add_argument("--prefix", help="Optional prefix for table names", default="")
     args = parser.parse_args()
 
-    main(args.suffix)
+    main(args.prefix)
--- utils/db_utils.py
+++ utils/db_utils.py
@@ -16,7 +16,8 @@ table_schema_rei_sweep = { # create_table appends primary_key, id (sequential)
         'page_limit': 'INTEGER',
         'condition': 'VARCHAR(20)',
         'page_n': 'INTEGER',
-        'run_id': 'TIMESTAMP',
+        'id': 'SERIAL PRIMARY KEY',
+        'run_id': 'INTEGER',
         'dt': 'TIMESTAMP',
     },
     'rei_sweep_all': {
@@ -31,8 +32,14 @@ table_schema_rei_sweep = { # create_table appends primary_key, id (sequential)
         'size_range': 'VARCHAR(50)[]', # 26 max obs
         'condition': 'VARCHAR(20)', # 19 max obs, code already breaks if condition names change
         'page_n': 'INTEGER',
-        'run_id': 'TIMESTAMP',
+        'id': 'SERIAL PRIMARY KEY',
+        'run_id': 'INTEGER',
         'dt': 'TIMESTAMP',
+    },
+    'rei_run' : {
+        'run_id': 'SERIAL PRIMARY KEY',
+        'run_name': 'VARCHAR(200) NOT NULL',
+        'run_dt': 'TIMESTAMP NOT NULL',
     },
 }
 
@@ -53,9 +60,8 @@ class DatabaseInserter:
     def create_table(self, table_name, columns, logger):
         # Define the columns and generate "create table" query
         column_definitions = [f"{column} {data_type}" for column, data_type in columns.items()]
-        column_definitions.append("id SERIAL PRIMARY KEY")
+        # column_definitions.append("id SERIAL PRIMARY KEY")
         create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(column_definitions)})"
-        
         try:
             with self.engine.connect() as connection:
                 connection.execution_options(isolation_level="AUTOCOMMIT")
@@ -64,22 +70,33 @@ class DatabaseInserter:
         except Exception as e:
             print(f"Error executing query {create_table_query} {e}")
             
-    def insert_to_sql(self, df: pd.DataFrame, table_name: str, helper_columns, logger):
+    def insert_to_sql(self, df: pd.DataFrame, table_name: str, logger, helper_columns=None):
         if df is None:
             logger.warning(f"No data to save to {table_name}. Skipping.")
             return
-        
-        # Add columns and values from helper_columns dictionary
-        for column, value in helper_columns.items():
-            df[column] = value
-
+        # Add optional helper_columns via dictionary
+        if helper_columns is not None:
+            for column, value in helper_columns.items():
+                df[column] = value
         # Save DataFrame to PostgreSQL table
         try:
             df.to_sql(table_name, self.engine, index=False, if_exists='append')
             logger.info(f"Saved {len(df)} rows to {table_name}")
         except Exception as e:
             logger.error(f"Error saving data to {table_name}: \n{traceback.format_exc()}")
-            breakpoint()
-    
+            
+    def start_run(self, run_name, prefix, logger):
+        run_dt = datetime.utcnow()
+        table_name = f"{prefix}rei_run"
+        insert_query = text(f"INSERT INTO {table_name} (run_dt, run_name) VALUES (:run_dt, :run_name) RETURNING run_id;")
+        try:
+            with self.engine.connect() as connection:
+                connection.execution_options(isolation_level="AUTOCOMMIT") # automatically commit insertions
+                result = connection.execute(insert_query, {'run_dt': run_dt, 'run_name': run_name})
+                run_id = result.fetchone()[0]
+                return run_id
+        except Exception as e:
+            logger.error(f"Error adding run {run_id} of {run_name} into {table_name}: {e} \n {insert_query}")
+        
     def close(self):
         self.engine.dispose()  # Close the database engine
